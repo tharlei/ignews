a8ce7270f75433dcce28168f4671d917596ff2cc
Tharlei Aleixo <tharlei.aleixo@grupofocus.com.br>
2022-03-02 23:08:20 -0300

    Consumir o stripe para exibir o valor do produto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,10 +25,10 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -27,3 +36,21 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KZ3ZKGCBJDPlJjZmSTiYJlM');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
