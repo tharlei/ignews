cb38b04c738916d8b91f5cbba63a1db6191c441d
Tharlei Aleixo <tharlei.aleixo@grupofocus.com.br>
2022-03-02 23:19:17 -0300

    Modificar de SSR para SSG carregamento da página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 import { stripe } from '../services/stripe';
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KZ3ZKGCBJDPlJjZmSTiYJlM');
 
   const product = {
@@ -52,5 +52,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
